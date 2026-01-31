f656e63c97bc1f493ccc14cdcb7fa626fa733969
Muin Chowdhury <muin.bd@gmail.com>
2026-01-31 19:03:26 +0600

    DB Design and Created DB Config with PDO Connection adn File Structure Added

---
+++ public/checkdb.php
@@ -0,0 +1,8 @@
+<?php
+$pdo = new PDO(
+    "mysql:host=127.0.0.1;port=3308;dbname=assignment_02_auth;charset=utf8mb4",
+    "root",
+    ""
+);
+
+echo "OK";
---
+++ public/test-db.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../app/Core/Database.php';
+
+$db = Database::getInstance();
+
+echo "Database connected successfully";
